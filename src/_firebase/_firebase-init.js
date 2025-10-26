fbd5ebb400f1ef6da406d1239c856d6720d61dd9
Solomon-77 <cyrilleustaquio111222@gmail.com>
2025-10-26 09:11:11 +0800

    lazy load firebase sdk

--- src/_firebase/_firebase-init.js
+++ src/_firebase/_firebase-init.js
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app"
-import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -9,7 +8,11 @@ const firebaseConfig = {
     messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
     appId: import.meta.env.VITE_FIREBASE_APP_ID,
     measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
-};
+}
 
-const app = initializeApp(firebaseConfig)
-export const analytics = getAnalytics(app)
+let firebaseApp = null
+
+export const getFirebaseApp = () => {
+    if (!firebaseApp) firebaseApp = initializeApp(firebaseConfig)
+    return firebaseApp
+}
--- src/components/AnalyticsTracker.jsx
+++ src/components/AnalyticsTracker.jsx
@@ -1,14 +1,14 @@
-import { useEffect } from "react"
-import { useLocation } from "react-router-dom"
-import { logEvent } from "firebase/analytics"
-import { analytics } from "../firebase"
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+import { getAnalyticsInstance } from '../_firebase/analytics-lazy'
 
 const AnalyticsTracker = () => {
     const location = useLocation()
 
     useEffect(() => {
-        logEvent(analytics, "screen_view", {
-            firebase_screen: location.pathname
+        requestIdleCallback(async () => {
+            const { analytics, logEvent } = await getAnalyticsInstance()
+            logEvent(analytics, 'screen_view', { firebase_screen: location.pathname })
         })
     }, [location])
 
