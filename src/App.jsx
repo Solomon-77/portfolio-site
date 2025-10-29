b3c5f686bce84c9e381b82ad913addb1e3ee1431
Solomon-77 <cyrilleustaquio111222@gmail.com>
2025-10-29 21:06:39 +0800

    remove firebase analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { LocationProvider } from 'preact-iso'
 import Layout from './components/Layout'
-import AnalyticsTracker from './components/AnalyticsTracker'
 
 const App = () => {
    return (
       <div className="font-custom scheme-dark">
          <LocationProvider>
-            <AnalyticsTracker />
             <Layout />
          </LocationProvider>
       </div>
