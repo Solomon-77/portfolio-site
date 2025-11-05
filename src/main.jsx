4c00e4723b006d04f02504e47c239b13f4333011
Solomon-77 <cyrilleustaquio111222@gmail.com>
2025-11-05 21:03:33 +0800

    prerender pages to add ssg

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,15 @@
-import { render } from 'preact'
+import { hydrate, prerender as ssr } from 'preact-iso'
 import './index.css'
 import App from './App.jsx'
 
-render(<App />, document.getElementById('root'))
+if (typeof window !== 'undefined') {
+    hydrate(<App />, document.getElementById('root'))
+}
+
+export async function prerender(data) {
+    const { html, links } = await ssr(<App {...data} />)
+    return {
+        html: `<div id="root">${html}</div>`,
+        links
+    }
+}
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    preact(),
+    preact({
+      prerender: { enabled: true }
+    }),
     tailwindcss()
   ],
 })
