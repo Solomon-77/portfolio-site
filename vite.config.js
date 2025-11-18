95496a75c9c7eb8fa02e3b884c77cb264f231203
Solomon-77 <cyrilleustaquio111222@gmail.com>
2025-11-18 19:49:44 +0800

    fix: add base path in vite config, routes, and navigating

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import preact from '@preact/preset-vite'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/portfolio-site/',
-  plugins: [
-    preact({
-      prerender: { enabled: true }
-    }),
-    tailwindcss()
-  ],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    base: env.VITE_BASE_URL || '/',
+    plugins: [
+      preact({
+        prerender: { enabled: true }
+      }),
+      tailwindcss()
+    ],
+  }
 })
